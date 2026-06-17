49cd69944aaa3a28f4b8c656a1b75ecbf11f8635
Atharva Mandle <atharvamandle19@gmail.com>
2026-06-17 15:12:08 +0530

    spi done

--- Student-api/main.go
+++ Student-api/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"encoding/json"
+	"fmt"
+	"strconv"
+	"net/http"
 )
 type Student struct{
 	Id int `json:"id"`
@@ -15,7 +16,8 @@ var studentsData = []Student{
 		{1,"Atharva",86,"Passes"},
 		{2, "Demo", 70, "Passed"},
 		{3, "Check", 35, "Failed"},
-	}
+}
+
 func hello(w http.ResponseWriter,r *http.Request){
 	fmt.Fprintln(w,"Method :",r.Method)
 }
@@ -29,12 +31,59 @@ func createStudent(w http.ResponseWriter, r *http.Request){
 	studentsData = append(studentsData,student)
 	json.NewEncoder(w).Encode(student)
 }
+func getStudent(w http.ResponseWriter, r *http.Request){
+	idstr := r.URL.Query().Get("id")
+	id,_:=strconv.Atoi(idstr)
+	var idStudent Student
+	for _,student:=range studentsData{
+		if student.Id==id {
+			idStudent=student
+		}
+	}
+	json.NewEncoder(w).Encode(idStudent)
+	
+}
+func deleteStudent(w http.ResponseWriter, r *http.Request){
+	idStr :=r.URL.Query().Get("id")
+	id,_:=strconv.Atoi(idStr)
+	
+	for index,student := range studentsData{
+		if student.Id==id {
+			studentsData=append( 
+			studentsData[:index],
+			studentsData[index+1:]...,)
+		}
+		
+	}
+	fmt.Fprintln(w,"student deleted")
+	
+}
+func updateStudent(w http.ResponseWriter,r *http.Request){
+	idStr:=r.URL.Query().Get("id")
+	id,_:=strconv.Atoi(idStr)
+	var updatedStudent Student
+	json.NewDecoder(r.Body).Decode(&updatedStudent)
+	for index,student:=range studentsData{
+		if student.Id==id {
+			studentsData[index].Marks=updatedStudent.Marks
+			studentsData[index].Name=updatedStudent.Name
+			studentsData[index].Status=updatedStudent.Status
+
+			json.NewEncoder(w).Encode(studentsData[index])
+			return
+		}
+	}
+	
+}
 func main(){
 	http.HandleFunc("/",hello)
 	http.HandleFunc("/students",students)
 	fmt.Println("server is runing at port: 8080")
 	http.HandleFunc("/createStudent",createStudent)
+	http.HandleFunc("/student",getStudent)
+	http.HandleFunc("/deleteStudent",deleteStudent)
+	http.HandleFunc("/updateStudent",updateStudent)
 	http.ListenAndServe(":8080",nil)
-
+	
 	
 }
